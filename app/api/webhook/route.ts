df7aa71fcff892ecc469c0a2235d85473ecb56c1
BishwasKoirala <wasubisu69@gmail.com>
2024-05-03 23:13:43 +0900

    webhook successfully ran

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,14 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
-
-import { NextResponse , NextRequest } from "next/server";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
 
-export async function POST(request : NextRequest , response : NextResponse) {
-  
-  const query = await request.json()
-  const siqniture = request.headers.get("Stripe-Signature")
+export async function POST(req : NextRequest) {
 
-  const dateTime = new Date(query.created * 1000).toLocaleDateString()
+  const payload = await req.text()
+
+  const response = JSON.parse(payload)
+  const sig = req.headers.get("Stripe-Signature")
+
+  const dateTime = new Date(response?.created * 1000).toLocaleDateString()
+  const timeString = new Date(response?.created * 1000).toLocaleTimeString()
+
+  try{
+    let event = stripe.webhooks.constructEvent(
+      payload,
+      sig!,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    )
+    console.log("event",event.type)
+    return NextResponse.json({status : 'succeed', event : event.type})
+    
+  } catch(error) {
+    return NextResponse.json({status : 'failed', error})
+  }
+
 
 }
